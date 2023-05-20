a87a870e72ef9b284d9d05444f8ff75ff9ab34b8
qorsan73 <99475446+qorsan73@users.noreply.github.com>
2023-05-21 01:39:56 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+TIMEOUT = 5
+MAX_CONNECTIONS = 900
+SORT_BY_SPEED = True
+SAVE_PATH = ""
+PROXIES = True
+PROXIES_ANONYMOUS = True
+PROXIES_GEOLOCATION = True
+PROXIES_GEOLOCATION_ANONYMOUS = True
+
+HTTP = True
+HTTP_SOURCES = (
+    "https://api.proxyscrape.com/v2/?request=getproxies&protocol=http",
+    "https://openproxy.space/list/http",
+    "https://raw.githubusercontent.com/almroot/proxylist/master/list.txt",
+    "https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt",
+    "https://raw.githubusercontent.com/hendrikbgr/Free-Proxy-Repo/master/proxy_list.txt",
+    "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-http%2Bhttps.txt",
+    "https://raw.githubusercontent.com/mmpx12/proxy-list/master/http.txt",
+    "https://raw.githubusercontent.com/mmpx12/proxy-list/master/https.txt",
+    "https://raw.githubusercontent.com/roosterkid/openproxylist/main/HTTPS_RAW.txt",
+    "https://raw.githubusercontent.com/sunny9577/proxy-scraper/master/proxies.txt",
+    "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt",
+    "https://raw.githubusercontent.com/UserR3X/proxy-list/main/http%2Bs.txt",
+    "https://raw.githubusercontent.com/Volodichev/proxy-list/main/http.txt",
+    "https://www.proxy-list.download/api/v1/get?type=http",
+    "https://www.proxy-list.download/api/v1/get?type=https",
+    "https://www.proxyscan.io/download?type=http",
+    "https://www.proxyscan.io/download?type=https",
+)
+SOCKS4 = True
+SOCKS4_SOURCES = (
+    "https://api.proxyscrape.com/v2/?request=getproxies&protocol=socks4",
+    "https://openproxy.space/list/socks4",
+    "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-socks4.txt",
+    "https://raw.githubusercontent.com/mmpx12/proxy-list/master/socks4.txt",
+    "https://raw.githubusercontent.com/roosterkid/openproxylist/main/SOCKS4_RAW.txt",
+    "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks4.txt",
+    "https://raw.githubusercontent.com/UserR3X/proxy-list/main/socks4.txt",
+    "https://www.proxy-list.download/api/v1/get?type=socks4",
+    "https://www.proxyscan.io/download?type=socks4",
+)
+SOCKS5 = True
+SOCKS5_SOURCES = (
+    "https://api.proxyscrape.com/v2/?request=getproxies&protocol=socks5",
+    "https://openproxy.space/list/socks5",
+    "https://raw.githubusercontent.com/hookzof/socks5_list/master/proxy.txt",
+    "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-socks5.txt",
+    "https://raw.githubusercontent.com/manuGMG/proxy-365/main/SOCKS5.txt",
+    "https://raw.githubusercontent.com/mmpx12/proxy-list/master/socks5.txt",
+    "https://raw.githubusercontent.com/roosterkid/openproxylist/main/SOCKS5_RAW.txt",
+    "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks5.txt",
+    "https://raw.githubusercontent.com/UserR3X/proxy-list/main/socks5.txt",
+    "https://www.proxy-list.download/api/v1/get?type=socks5",
+    "https://www.proxyscan.io/download?type=socks5",
+)
